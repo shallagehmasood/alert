8afdd53567e5c78dc94148577325b3dd20696b41
shallagehmasood <shallagehmasood@gmail.com>
2025-10-30 11:44:06 +0330

    Update signal_provider.dart

--- lib/providers/signal_provider.dart
+++ lib/providers/signal_provider.dart
@@ -6,9 +6,16 @@ class SignalProvider with ChangeNotifier {
   final WebSocketService _webSocketService = WebSocketService();
   List<Signal> _signals = [];
   bool _isConnected = false;
+  Signal? _latestSignal;
 
   List<Signal> get signals => _signals;
   bool get isConnected => _isConnected;
+  Signal? get latestSignal => _latestSignal;
+
+  void clearLatestSignal() {
+    _latestSignal = null;
+    notifyListeners();
+  }
 
   void connect(String userId) {
     _webSocketService.connect(userId);
@@ -27,6 +34,8 @@ class SignalProvider with ChangeNotifier {
 
   void _addSignal(Signal signal) {
     _signals.insert(0, signal);
+    _latestSignal = signal;
+    
     if (_signals.length > 100) {
       _signals = _signals.sublist(0, 100);
     }
@@ -35,6 +44,7 @@ class SignalProvider with ChangeNotifier {
 
   void clearSignals() {
     _signals.clear();
+    _latestSignal = null;
     notifyListeners();
   }
 
