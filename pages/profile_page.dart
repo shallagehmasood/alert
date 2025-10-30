4ebc24ee35e1827647777ef829628eb5fade7b87
shallagehmasood <shallagehmasood@gmail.com>
2025-10-30 04:55:32 +0330

    Create profile_page.dart

---
+++ pages/profile_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'صفحه پروفایل',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontFamily: 'Vazir',
+        ),
+      ),
+    );
+  }
+}
