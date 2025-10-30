e8af33e558b9853b1f19a14bd73a6d66684e6821
shallagehmasood <shallagehmasood@gmail.com>
2025-10-30 04:57:03 +0330

    Create pairs_page.dart

---
+++ lib/pages/pairs_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PairsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'صفحه جفت‌ارزها',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontFamily: 'Vazir',
+        ),
+      ),
+    );
+  }
+}
