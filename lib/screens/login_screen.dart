07d48b9286f7b2c176bc4b109cd6c06292eace2d
shallagehmasood <shallagehmasood@gmail.com>
2025-10-30 09:22:15 +0330

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/settings_provider.dart';
+import 'home_screen.dart'; // اضافه کردن این import
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
