4884af1828970a1ecd077875c4f6174f20e0ca38
shallagehmasood <shallagehmasood@gmail.com>
2025-10-30 04:37:35 +0330

    Create signals_page.dart

---
+++ lib/pages/signals_page.dart
@@ -0,0 +1,39 @@
+// pages/signals_page.dart
+class SignalsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ApiService>(
+      builder: (context, apiService, child) {
+        final signals = apiService.signals;
+        
+        if (signals.isEmpty) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.show_chart, size: 64, color: Color(0xFFB0B0B0)),
+                SizedBox(height: 16),
+                Text(
+                  'سیگنالی یافت نشد',
+                  style: TextStyle(
+                    color: Color(0xFFB0B0B0),
+                    fontSize: 16,
+                    fontFamily: 'Vazir',
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+
+        return ListView.builder(
+          padding: EdgeInsets.all(16),
+          itemCount: signals.length,
+          itemBuilder: (context, index) {
+            return SignalCard(signal: signals[index]);
+          },
+        );
+      },
+    );
+  }
+}
