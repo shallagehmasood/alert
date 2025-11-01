427f09dbb35fee094be441ca55d04b7b7cd1abaa
shallagehmasood <shallagehmasood@gmail.com>
2025-11-01 07:45:06 +0330

    Delete lib directory

--- lib/providers/signal_provider.dart
+++
@@ -1,6 +0,0 @@
-// اضافه کردن این متد به کلاس SignalProvider
-void removeSignalFromApp(Signal signal) {
-  _signals.remove(signal);
-  notifyListeners();
-  print('🗑️ سیگنال از اپلیکیشن حذف شد: ${signal.pair} - ${signal.timeframe}');
-}
